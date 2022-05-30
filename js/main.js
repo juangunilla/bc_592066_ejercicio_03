ea978de26ef1357752393e481e47dc9be249189e
Gabriel Torres <gabrieltorresmora2@gmail.com>
2022-05-30 11:09:19 -0300

    Se ha añadido un proyecto completo

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nombre = prompt('ingrese  su nombre')
+
+console.log(nombre)
+
+let edad = prompt('Ingrese su edad')
+
+if (edad <30) {
+    console.log('Hola, como andas')
+    alert('Hola, como andas')
+} else {
+    console.log('Bienvenido.Buenos dias')
+    alert('Bienvenido.Buenos dias')
+}
